7e7c06b16ebb2f88258383343e2d50d491eb694f
Njabulo Sibiya <75039043+njabulosibiya@users.noreply.github.com>
2022-12-10 15:44:27 +0200

    Created sub-directories for header and resources. Created .h file for all non-class functions to be implemented in main.

---
+++ revision/revision.cpp
@@ -0,0 +1,15 @@
+/*Author: Njabulo k Sibiya
+* contact: ilovenjabs@gmail.com
+* created: 26 Nov 2022
+*/
+
+#include <iostream>
+#include <fstream>
+#include "headers/fileInput.h"
+
+int main() {
+	std::ifstream file1;
+
+	fileData(file1);
+	return 0;
+}
